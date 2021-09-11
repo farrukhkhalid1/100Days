5f52d7bd63a2bc4e3c687e0e54ba7ae9df72e9e3
farrukhkhalid1 <farrukhkhalid1@gmail.com>
2021-09-11 15:58:30 +0200

    Day 6 Project

---
+++ Day 6/main.py
@@ -0,0 +1,20 @@
+# Maze -Reeborg.ca
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    return 0
+
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        if front_is_clear():
+            move()
+    elif wall_in_front():
+        turn_left()
+        if front_is_clear():
+            move()
+    elif front_is_clear():
+        move()
+
